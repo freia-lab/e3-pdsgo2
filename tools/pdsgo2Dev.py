6ea7c010a5ec98546080699f661cf9847f7b2c99
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-01-21 20:49:24 +0100

    Removed unneeded init code. Changed scaling factor for dose rate.

--- tools/pdsgo2Dev.py
+++ tools/pdsgo2Dev.py
@@ -12,28 +12,13 @@ class Pdsgo2Dev:
         print("========== Initializing USB device ===============\n", dev)
         msg = bytearray()
         assert dev.ctrl_transfer(0x41, 0, 0x001, 0, msg) == len(msg)
-        msg = bytearray()
-        assert dev.ctrl_transfer(0x41, 3, 0x0800, 0, msg) == len(msg)
-        msg = bytearray([0,0,0,0,0,0,0,0,0,0x80,0,0,0,0x20,0,0])
-        assert dev.ctrl_transfer(0x41, 19, 0x0, 0, msg) == len(msg)
-        msg = bytearray([0,0,0,0,0x11, 0x13])
-        assert dev.ctrl_transfer(0x41, 25, 0, 6, msg) == len(msg)
         msg = bytearray([0,0xc2,1,0])
         assert dev.ctrl_transfer(0x41, 30, 0, 4, msg) == len(msg)
-#        msg = bytearray([0,0xc2,1,0])
-#        assert dev.ctrl_transfer(0x41, 30, 0, 4, msg) == len(msg)
         #frame 54
         msg = bytearray()
         assert dev.ctrl_transfer(0x41, 7, 0x0200, 0, msg) == len(msg)
         msg = bytearray()
         assert dev.ctrl_transfer(0x41, 7, 0x0100, 0, msg) == len(msg)
-        msg = bytearray()
-        assert dev.ctrl_transfer(0x41, 3, 0x0800, 0, msg) == len(msg)
-        msg = bytearray([0x1a,0,0,0x1a,0x11,0x13])
-        assert dev.ctrl_transfer(0x41, 25, 0, 6, msg) == len(msg)
-        #frame 62
-        msg = bytearray([0,0,0,0,0,0,0,0,0x80,0,0,0,0x80,0,0,0])
-        assert dev.ctrl_transfer(0x41, 19, 0x0, 0, msg) == len(msg)
         msg = bytearray([0,0xc2,1,0])
         assert dev.ctrl_transfer(0x41, 30, 0, 4, msg) == len(msg)
         msg = bytearray()
@@ -46,8 +31,6 @@ class Pdsgo2Dev:
         assert dev.ctrl_transfer(0x41, 25, 0, 6, msg) == len(msg)
         msg = bytearray([0,0,0,0,0,0,0,0,0x80,0,0,0,0x80,0,0,0])
         assert dev.ctrl_transfer(0x41, 19, 0x0, 0, msg) == len(msg)
-#        msg = bytearray()
-#        assert dev.ctrl_transfer(0x41, 7, 0x0100, 0, msg) == len(msg)
 
     def getdata(self):
         if self.dev == None:
