1cc47a248166c3fa1a649097e4ec84d6397ff41c
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-01-26 09:14:53 +0100

    Updated the database and the opi.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -4,5 +4,5 @@ require pdsgo2
 
 epicsEnvSet ("IOCNAME", "ioc05-pdsgo2")
 
-iocshLoad("$(pdsgo2_DIR)/pdsgo2.iocsh", ",IP_ADDR=localhost,IP_PORT=1140, ASYN_PORT=PORT1")
+iocshLoad("$(pdsgo2_DIR)/pdsgo2.iocsh", ",IP_ADDR=nuc-03,IP_PORT=1140, ASYN_PORT=PORT1")
 
